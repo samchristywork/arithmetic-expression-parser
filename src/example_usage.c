0824ccbcffea76d75655ae4b336fb66010fa9152
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-07-19 21:36:22 -0500

    Added example program.
    This program shows how one would use the library made up of eval.c and
    eval.h.

---
+++ src/example_usage.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "eval.h"
+
+int main() {
+  char buf[1024];
+  while (1) {
+    printf("Enter an expression: ");
+    int ret = scanf("%s", buf);
+    if (ret == -1) {
+      break;
+    }
+    int result = 0;
+    eval(buf, &result);
+    printf("Answer: %d\n\n", result);
+  }
+}
