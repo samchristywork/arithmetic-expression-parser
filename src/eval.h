accf0885e1fb45cbe2ba3e34cb6e1c6524a9650a
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-07-31 23:01:20 -0500

    Added documentation to header file.
    This is the documentation the user needs to read to understand how to
    use this library.

--- src/eval.h
+++ src/eval.h
@@ -1,6 +1,29 @@
 #ifndef EVAL_H
 #define EVAL_H
 
+/*
+ * Evaluate a string
+ *
+ * Parameters:
+ *   string: A null-terminated string of characters representing an arithmetic expression.
+ *   result: A pointer to the integer result of the evaluated expression.
+ *
+ * Return value:
+ *   EXIT_SUCCESS if the string was successfully evaluated,
+ *   EXIT_FAILURE otherwise.
+ *
+ * Example:
+ *
+ *   ```
+ *   int result = 0;
+ *   int ret = eval("1+2", &result);
+ *   if(ret) {
+ *     printf("Answer: %d\n", result);
+ *   } else {
+ *     printf("Syntax error.\n");
+ *   }
+ *   ```
+ */
 int eval(const char *string, int *result);
 
 #endif
