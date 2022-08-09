7c2ad711995c6ad4d00ae0fbb7f643cc6c6de90f
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-08-08 20:05:06 -0500

    Added comments.

--- src/example_usage.c
+++ src/example_usage.c
@@ -6,14 +6,26 @@
 int main() {
   char buf[1024];
   while (1) {
+
+    /*
+     * Read the user's input.
+     */
     printf("Enter an expression: ");
     int ret = scanf("%s", buf);
     if (ret == -1) {
       printf("\n");
       break;
     }
+
+    /*
+     * Evaluate the user input using the "eval" function.
+     */
     int result = 0;
     eval(buf, &result);
+
+    /*
+     * Print the value of the evaluated expression.
+     */
     printf("Answer: %d\n\n", result);
   }
 }
